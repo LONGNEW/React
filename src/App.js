1a0fdc5853aebce5c983c3782e28c7c7a15d7d53
ShinHyoung <jsin2475@naver.com>
2022-01-08 17:40:37 +0900

    #6.1 function to run it once
    [useEffect()]
    : make a function to run it once

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the api");
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Save"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
