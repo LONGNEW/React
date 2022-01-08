ace70cf41be453cba3a0632ebc791783cc13c8ac
ShinHyoung <jsin2475@naver.com>
2022-01-08 23:08:44 +0900

    # 7.1 making <li>s with map()
    you have to give key attribute when using array

--- src/App.js
+++ src/App.js
@@ -9,26 +9,26 @@ function App() {
     if (toDo === "") {
       return;
     }
-    // 현재 함수를 빠져나가게 되면 setter의 영향을 받는 듯.
     setToDo("");
-    setToDos(currentArray => [toDo, ...currentArray]);
-    // setToDos(function (currentArray) {
-    //   return [toDo, ...currentArray]
-    // })
-
+    setToDos((currentArray) => [toDo, ...currentArray]);
   };
-  
-  useEffect(() => {
-    console.log(toDos);
-  }, [toDos]);
 
   return (
     <div>
       <h1>My To Dos {toDos.length}</h1>
+
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} type={"text"} placeholder="Write your to do..." />
         <button>Add to do</button>
       </form>
+
+      <hr />
+
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
