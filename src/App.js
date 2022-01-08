12ce75f986537b290180a54bd7b4e88d7e061a13
ShinHyoung <jsin2475@naver.com>
2022-01-08 22:48:18 +0900

    # 7.0 using setter to push array

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  
-  function destroyedFn() {
-    console.log("destroyed");
-  }
-  // 처음 생성될 때를 체크 하기 때문에 이 컴포넌트가 삭제되는 것도 감지를 함.
-  function effectFn() {
-    console.log("created");
-
-    // 삭제 될 때 동작하도록 하려면 return 문을 사용함.
-    return destroyedFn;
-  }
-  
-  useEffect(effectFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    // 현재 함수를 빠져나가게 되면 setter의 영향을 받는 듯.
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+    // setToDos(function (currentArray) {
+    //   return [toDo, ...currentArray]
+    // })
+
+  };
+  
+  useEffect(() => {
+    console.log(toDos);
+  }, [toDos]);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type={"text"} placeholder="Write your to do..." />
+        <button>Add to do</button>
+      </form>
     </div>
   );
 }
