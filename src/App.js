ada04805644849074a455a755d01b91d4a4b23b0
ShinHyoung <jsin2475@naver.com>
2022-01-08 17:54:31 +0900

    #6.2 [] is for variable to check the changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((prev) => prev + 1);
+  const [keyword, setKeyword] = useState("");
 
-  console.log("i run all the time");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  
   useEffect(() => {
     console.log("Call the api");
   }, []);
+  // keyword의 변화를 감지해서 실행.
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      console.log("SEARCH : ", keyword);
+  }, [keyword]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type={"text"}
+        placeholder="Search"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
