5a6c67a4ed840d1d1ef4032ee9b1325d913eb004
ShinHyoung <jsin2475@naver.com>
2022-01-09 19:31:52 +0900

    # 7.2 fetch to get the info
    use ternary operation to code easier

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
+  const [load, setLoad] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setLoad(false);
+        setCoins(json);
+      });
+  }, []);
 
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type={"text"} placeholder="Write your to do..." />
-        <button>Add to do</button>
-      </form>
-
-      <hr />
-
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>Coin {load ? "" : `(${coins.length})`}</h1>
+      {load ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li>
+              {coin.name} ({coin.symbol}) : $ {coin.quotes.USD.price} USD
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
